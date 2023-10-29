9d5514b40861fc66f80bed279806970b024bcbb0
ritikparte01 <ritikparte01>
2023-10-29 12:35:03 +0000

    First Commit from Locofy Builder

---
+++ src/pages/Frame.js
@@ -0,0 +1,208 @@
+import { useCallback } from "react";
+import styles from "./Frame.module.css";
+
+const Frame = () => {
+  const onOpenText1Click = useCallback(() => {
+    // Please sync "Desktop - 2" to the project
+  }, []);
+
+  return (
+    <div className={styles.desktop1Parent}>
+      <div className={styles.desktop1}>
+        <img
+          className={styles.pngtreeshoppingLogoDeisgnM}
+          alt=""
+          src="/pngtreeshopping-logo-deisgn-modern-style-8549828-1@2x.png"
+        />
+        <b className={styles.home}>Home</b>
+        <div className={styles.aboutUs}>About Us</div>
+        <div className={styles.products}>Products</div>
+        <div className={styles.contact}>Contact</div>
+        <div className={styles.desktop1Child} />
+        <div className={styles.desktop1Item} />
+        <div className={styles.desktop1Inner} />
+        <img
+          className={styles.blackAndWhiteSilhouetteMot}
+          alt=""
+          src="/black-and-white-silhouette-motivational-quotes-facebook-cover-1@2x.png"
+        />
+        <div className={styles.theBestOnline}>The Best Online store</div>
+        <div className={styles.ultrawideMonitor}>43” UltraWide Monitor</div>
+        <div className={styles.mensFashion}>Men’s Fashion</div>
+        <div className={styles.loremIpsumDolorContainer}>
+          <span className={styles.loremIpsumDolorContainer1}>
+            <p className={styles.loremIpsumDolor}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores
+              odio ea neque
+            </p>
+          </span>
+        </div>
+        <div className={styles.loremIpsumDolorContainer2}>
+          <span className={styles.loremIpsumDolorContainer1}>
+            <p className={styles.loremIpsumDolor}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores
+              odio ea neque dolor sit amet consectetur adipisicing
+            </p>
+          </span>
+        </div>
+        <div className={styles.loremIpsumDolorContainer4}>
+          <span className={styles.loremIpsumDolorContainer1}>
+            <p className={styles.loremIpsumDolor}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing.
+            </p>
+          </span>
+        </div>
+        <div className={styles.allTrandyClothings}>
+          All Trandy Clothings for mens..!!!
+        </div>
+        <div className={styles.div}>01.</div>
+        <div className={styles.div1}>02.</div>
+        <div className={styles.ecommerce}>Ecommerce.</div>
+        <img
+          className={styles.zt42iocgl1Icon}
+          alt=""
+          src="/81zt42iocgl-1@2x.png"
+        />
+        <img className={styles.menb1Icon} alt="" src="/menb-1@2x.png" />
+        <img
+          className={styles.playButton1Icon}
+          alt=""
+          src="/playbutton-1@2x.png"
+        />
+        <img className={styles.lineIcon} alt="" src="/line-1.svg" />
+        <div className={styles.specialDiscountUpContainer}>
+          <span className={styles.loremIpsumDolorContainer1}>
+            <span className={styles.specialDiscount}>
+              <span>SPECIAL DISCOUNT</span>
+              <span className={styles.span}>{` `}</span>
+            </span>
+            <b className={styles.upTo50}>UP TO 50% OFF</b>
+            <span className={styles.specialDiscount}> FOR ALL ITEMS</span>
+          </span>
+        </div>
+        <div className={styles.sellingOnlyTheContainer}>
+          <span className={styles.loremIpsumDolorContainer1}>
+            <b>SELLING</b>
+            <span className={styles.onlyThe}>{` ONLY THE `}</span>
+            <b className={styles.span}>BEST</b>
+            <span className={styles.onlyThe}>{` THINGS `}</span>
+            <b>ONLINE</b>
+          </span>
+        </div>
+        <div className={styles.rectangleDiv} />
+        <div className={styles.desktop1Child1} />
+        <div className={styles.availableAt31stContainer}>
+          <span className={styles.loremIpsumDolorContainer1}>
+            <p className={styles.availableAt}>AVAILABLE AT</p>
+            <p className={styles.stDec2022}>
+              <b>31ST DEC 2022</b>
+            </p>
+            <p className={styles.availableAt}>CHECK OUR WEBSITE...!!</p>
+          </span>
+        </div>
+        <img
+          className={styles.giorgioTrovato7bw3reikli0UnIcon}
+          alt=""
+          src="/giorgiotrovato7bw3reikli0unsplash-1@2x.png"
+        />
+        <img
+          className={styles.julioAngelBerroaNvbqcnat2qIcon}
+          alt=""
+          src="/julioangelberroanvbqcnat-2qunsplash-1@2x.png"
+        />
+        <img
+          className={styles.tamannaRumeeLpgm415q9jaUnspIcon}
+          alt=""
+          src="/tamannarumeelpgm415q9jaunsplash-1@2x.png"
+        />
+        <div className={styles.category}>Category</div>
+        <div className={styles.price}>Price</div>
+        <div className={styles.desktop1Child2} />
+        <div className={styles.desktop1Child3} />
+        <div className={styles.desktop1Child4} />
+        <div className={styles.desktop1Child5} />
+        <div className={styles.desktop1Child6} />
+        <div className={styles.desktop1Child7} />
+        <img
+          className={styles.julioAngelBerroaNvbqcnat2qIcon1}
+          alt=""
+          src="/julioangelberroanvbqcnat-2qunsplash-15@2x.png"
+        />
+        <img
+          className={styles.julioAngelBerroaNvbqcnat2qIcon2}
+          alt=""
+          src="/julioangelberroanvbqcnat-2qunsplash-15@2x.png"
+        />
+        <img
+          className={styles.julioAngelBerroaNvbqcnat2qIcon3}
+          alt=""
+          src="/julioangelberroanvbqcnat-2qunsplash-16@2x.png"
+        />
+        <img
+          className={styles.julioAngelBerroaNvbqcnat2qIcon4}
+          alt=""
+          src="/julioangelberroanvbqcnat-2qunsplash-16@2x.png"
+        />
+        <img
+          className={styles.julioAngelBerroaNvbqcnat2qIcon5}
+          alt=""
+          src="/julioangelberroanvbqcnat-2qunsplash-17@2x.png"
+        />
+        <img
+          className={styles.julioAngelBerroaNvbqcnat2qIcon6}
+          alt=""
+          src="/julioangelberroanvbqcnat-2qunsplash-17@2x.png"
+        />
+        <div className={styles.product}>PRODUCT!</div>
+        <div className={styles.product1}>PRODUCT!</div>
+        <div className={styles.product2}>PRODUCT!</div>
+        <div className={styles.product3}>PRODUCT!</div>
+        <div className={styles.product4}>PRODUCT!</div>
+        <div className={styles.product5}>PRODUCT!</div>
+        <div className={styles.vectorParent}>
+          <img className={styles.groupChild} alt="" src="/rectangle-27.svg" />
+          <div className={styles.open}>Open</div>
+        </div>
+        <div className={styles.vectorGroup}>
+          <img className={styles.groupChild} alt="" src="/rectangle-27.svg" />
+          <div className={styles.open1} onClick={onOpenText1Click}>
+            Open
+          </div>
+        </div>
+        <div className={styles.vectorContainer}>
+          <img className={styles.groupChild} alt="" src="/rectangle-27.svg" />
+          <div className={styles.open}>Open</div>
+        </div>
+        <div className={styles.groupDiv}>
+          <img className={styles.groupChild} alt="" src="/rectangle-27.svg" />
+          <div className={styles.open}>Open</div>
+        </div>
+        <div className={styles.vectorParent1}>
+          <img className={styles.groupChild} alt="" src="/rectangle-27.svg" />
+          <div className={styles.open}>Open</div>
+        </div>
+        <div className={styles.vectorParent2}>
+          <img className={styles.groupChild} alt="" src="/rectangle-27.svg" />
+          <div className={styles.open}>Open</div>
+        </div>
+        <div className={styles.desktop1Child8} />
+        <b className={styles.seeMore}>See More</b>
+        <div className={styles.desktop1Child9} />
+        <div className={styles.subscribeForEmail}>
+          Subscribe for Email Updates. Unsubscribe anytime.
+        </div>
+        <div className={styles.desktop1Child10} />
+        <div className={styles.yourEmailAddress}>Your email address</div>
+        <div className={styles.desktop1Child11} />
+        <div className={styles.submit}>Submit</div>
+        <div className={styles.desktop1Child12} />
+        <div className={styles.desktop1Child13} />
+        <div className={styles.desktop1Child14} />
+        <div className={styles.desktop1Child15} />
+        <img className={styles.download21} alt="" src="/download-2-1@2x.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Frame;
